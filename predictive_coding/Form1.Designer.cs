48e05f04dc3b1db6f802a595fa760e730c81e99b
nicu <nicolae.taban@gmail.com>
2020-03-15 01:10:28 +0200

    Load original image

--- predictive_coding/Form1.Designer.cs
+++ predictive_coding/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace predictive_coding
 {
-    partial class Form1
+    partial class PredictiveCodingForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.OriginalImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.CoderLoadButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImagePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // OriginalImagePictureBox
+            // 
+            this.OriginalImagePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OriginalImagePictureBox.Location = new System.Drawing.Point(12, 12);
+            this.OriginalImagePictureBox.Name = "OriginalImagePictureBox";
+            this.OriginalImagePictureBox.Size = new System.Drawing.Size(256, 256);
+            this.OriginalImagePictureBox.TabIndex = 0;
+            this.OriginalImagePictureBox.TabStop = false;
+            // 
+            // CoderLoadButton
+            // 
+            this.CoderLoadButton.Location = new System.Drawing.Point(12, 274);
+            this.CoderLoadButton.Name = "CoderLoadButton";
+            this.CoderLoadButton.Size = new System.Drawing.Size(75, 23);
+            this.CoderLoadButton.TabIndex = 1;
+            this.CoderLoadButton.Text = "Load";
+            this.CoderLoadButton.UseVisualStyleBackColor = true;
+            this.CoderLoadButton.Click += new System.EventHandler(this.CoderLoadButton_Click);
+            // 
+            // PredictiveCodingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.CoderLoadButton);
+            this.Controls.Add(this.OriginalImagePictureBox);
+            this.Name = "PredictiveCodingForm";
+            this.Text = "Predictive Coding";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImagePictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox OriginalImagePictureBox;
+        private System.Windows.Forms.Button CoderLoadButton;
     }
 }
 
--- predictive_coding/Form1.cs
+++ predictive_coding/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,9 @@ using System.Windows.Forms;
 
 namespace predictive_coding
 {
-    public partial class Form1 : Form
+    public partial class PredictiveCodingForm : Form
     {
-        public Form1()
+        public PredictiveCodingForm()
         {
             InitializeComponent();
         }
@@ -20,6 +21,31 @@ namespace predictive_coding
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void CoderLoadButton_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            Bitmap bitmapImage = null;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "BMP FIles (*.bmp)|*.BMP";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    bitmapImage = new Bitmap(filePath);
+                    OriginalImagePictureBox.Image = bitmapImage;
+                   
+                }
+            }
         }
     }
 }

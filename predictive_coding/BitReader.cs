54c812947652f2ade3bfb66dee266c2debbbca16
nicu <nicolae.taban@gmail.com>
2020-03-15 00:31:47 +0200

    Initial commit

---
+++ predictive_coding/BitReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitReaderWriter
+{
+    class BitReader
+    {
+        byte readBuffer;
+        long readBitsCounter;
+        FileStream fileStream;
+        BinaryReader reader;
+
+        public BitReader(string filePath)
+        {
+            fileStream = new FileStream(filePath, FileMode.Open);
+            reader = new BinaryReader(fileStream);
+            readBitsCounter = 0;
+        }
+
+        public void closeFile()
+        {
+            reader.Dispose();
+            fileStream.Dispose();
+        }
+
+        public int readBit()
+        {
+            if(readBitsCounter % 8 == 0)
+            {
+                readBuffer = reader.ReadByte();
+            }
+            int position = 7 - (int)(readBitsCounter % 8);
+            int mask = 1 << position;
+            int bit = readBuffer & mask;
+            bit = bit >> position;
+            readBitsCounter++;
+            return bit;
+        }
+
+        public int readNBits(int numberOfBits)
+        {
+            int position = numberOfBits - 1;
+            int result = 0;
+            for(int i = position; i >= 0; i--)
+            {
+                int bit = readBit();
+                bit = bit << i;
+                result = result | bit;
+            }
+            return result;
+        }
+    }
+}
---
+++ predictive_coding/BitWriter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitReaderWriter
+{
+    class BitWriter
+    {
+        byte writeBuffer;
+        long writtenBitsCounter;
+        FileStream fileStream;
+        BinaryWriter writer;
+
+        public BitWriter(string filePath)
+        {
+            fileStream = new FileStream(filePath, FileMode.Create);
+            writer = new BinaryWriter(fileStream);
+            writtenBitsCounter = 0;
+        }
+
+        public void closeFile()
+        {
+            writer.Dispose();
+            fileStream.Dispose();
+        }
+
+        public void writeBit(int bit)
+        {
+            int lsb = bit & 1; // get the least significant bit
+            lsb = lsb << 7 - (int)(writtenBitsCounter % 8);
+            writeBuffer = (byte)(writeBuffer | lsb);
+            writtenBitsCounter++;
+            if (writtenBitsCounter % 8 == 0)
+            {
+                writer.Write(writeBuffer);
+                writeBuffer = 0;
+            }
+        }
+
+        public void writeNBits(int payload, int numberOfBits)
+        {
+            int position = numberOfBits - 1;
+            for(int i = position; i >= 0; i--)
+            {
+                int mask = 1 << i;
+                int bit = payload & mask;
+                bit = bit >> i;
+                writeBit(bit);
+            }
+        }
+
+    }
+}
